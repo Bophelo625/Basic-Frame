0b082d7729f8718f829f3bf28f4a75123abcabcf
Bophelo625 <224220367@tut4life.ac.za>
2024-08-01 11:30:50 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,16 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+
+public class BasicFrameApp {
+
+    
+    public static void main(String[] args) {
+        MyFirstFrame mff;
+        
+        mff= new MyFirstFrame();
+    }
+    
+}
